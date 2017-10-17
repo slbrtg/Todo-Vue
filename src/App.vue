2807a23bad8593f6b2aff57c5acdb2adf747e8bf
saul <slbrtg@gmail.com>
2017-10-16 20:15:51 -0700

    add edit and delete options to each task

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
 
 export default {
   name: 'app',
   components: {
     TodoList,
+    CreateTodo,
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title,
+        done: false,
+      });
+    },
   },
   // data function avails data to the template
   data() {
